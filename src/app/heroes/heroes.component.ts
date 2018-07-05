0d69c4c327b265cbf9dad242c675e54e2b8f5fdf
crystallove <crystallove122@gmail.com>
2018-07-05 11:51:31 +0700

    Create a service, preparing for a  real work (get data from remote server not mock data source) - Use reactive library js _ observable and subcribe to get data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,19 +9,20 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-
-  
-
-  heroes = HEROES; 
+  heroes : Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService : HeroService) { }
    
   ngOnInit() {
-    
+    this.getHeroes();  
   }
 
   onSelected(hero:Hero):void{
     this.selectedHero = hero;
   }
 
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes =heroes);
+  }
 }
