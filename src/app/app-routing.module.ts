24b8dada4b4b41534c56ceaddede5307d518debb
crystallove <crystallove122@gmail.com>
2018-07-05 16:38:37 +0700

    Added Messages and create a dashboard view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,27 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HeroesComponent} from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component'
 
 
 const routes: Routes =[
   {
+    //adding default router to navigate to dashboard as index
+    path:'',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ]
+
+
 
 @NgModule({
   imports: [
